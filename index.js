549ee4bd93081f09a9b37630ff1a87f5079b7c97
Lance Hunter <lancehunter@gmail.com>
2017-11-07 12:26:25 -0600

    Made it possible to draw by holding mouse down.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
   let board = document.getElementById('board');
   let pallete = document.getElementById('pallete');
   let current = document.getElementById('current');
+  let brush = false;
 
   let theColor = '';
 
@@ -9,12 +10,33 @@
     let pixel = document.createElement('div');
     pixel.classList.add('pixel');
     pixel.id = `pixel${i}`;
-//    pixel.innerText = `Pixel: ${i}`;
     board.appendChild(pixel);
   }
 
   palette.addEventListener('click', selectColor);
   board.addEventListener('click', changeColor);
+  board.addEventListener('mousedown', brushColor);
+
+
+  function brushColor() {
+    board.addEventListener('mouseover', startBrush);
+  }
+
+  function startBrush() {
+    board.addEventListener('mouseup', endBrush);
+    let square = document.getElementById(event.target.id);
+    if (theColor) {
+      square.style.backgroundColor = theColor;
+      square.style.borderColor = theColor;
+    }
+    if (theColor === 'white') {
+      square.style.borderColor = 'silver';
+    }
+  }
+
+  function endBrush() {
+    board.removeEventListener('mouseover', startBrush);
+  }
 
   function selectColor() {
     let newColor = event.target.id;
